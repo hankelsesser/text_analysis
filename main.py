271714f1f864555c6bd19462e3d33175d7d1bcc0
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-11 10:38:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ def get_info(string):
     for i in (len(string)):
         song = ""
         firstq = string[i].find('"')
+        #find author
         song += string[i][:firstq]
         song += "-"
         #find title
         song += string[i][firstq:(string[i].find('"', firstq+1))]
         song+="  "
+        #find number of weeks
         song+= string[i][string[i].find('"', firstq+1):string[i].find("n")-1]
 
         string.append(song)

46050ca3e2c7bf4480eb930b8b1986598451a3ba
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-17 16:53:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def get_length_sucess_data(file, option):
 
 def create_graph(x, y):
     #plt.plot('length of title', 'weeks at #1', data=dataset)
-    plt.plot(x, y)
+    plt.stem(x, y)
     plt.xlabel("lenth of title")
     plt.ylabel("weeks at #1")
     plt.show()

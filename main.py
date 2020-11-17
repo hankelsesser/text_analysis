98d6cd925491a18cfb57994f34f2528e8f259304
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-17 15:21:18 -0500

    next

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ def get_songs(file):
 rawsongs = get_songs("songs2010.txt")
 
 def get_info(string):
-    songs = []
-    for i in range(len(string)):
-        song = []
-        firstq = string[i].find('"')
-        #find author
-        song.append(string[i][:firstq-1])
-        #find title
-        song.append(string[i][firstq+1:(string[i].find('"', firstq+1))])
-        #find number of weeks
-        print(string)
-        song.append(string[i][string[i].find('"', firstq+1)+1:string[i].find("n")])
+    songs = string.split('/n')
+    for i in range(len(songs)):
+        songs[i] = songs[i].split()
 
-        songs.append(song)
+    # for i in range(len(string)):
+    #     song = []
+    #     firstq = string[i].find('"')
+    #     #find author
+    #     song.append(string[i][:firstq-1])
+    #     #find title
+    #     song.append(string[i][firstq+1:(string[i].find('"', firstq+1))])
+    #     #find number of weeks
+    #     song.append(string[i][string[i].find('"', firstq+1)+1:string[i].find("n")])
+    #
+    #     songs.append(song)
+
     return(songs)
 
 print(get_info(rawsongs))

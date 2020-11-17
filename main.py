3e31c7bc90a67153f1a8e647dc6c6910e822e2eb
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-17 15:35:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-def get_songs(file):
+
+songs1980 = getsong("songs1980.txt")
+songs1990 = getsong("songs1990.txt")
+songs2000 = getsong("songs2000.txt")
+songs2010 = getsong("songs2010.txt")
+
+def get_song(file):
     songs = open(file).readlines()
-    return(songs)
-rawsongs = get_songs("songs2010.txt")
+    return(get_info(songs))
 
 def get_info(string):
     songs = []
-    # for i in range(len(songs)):
-    #     songs[i] = songs[i].split()
-
     for i in range(len(string)):
         song = []
+        #location of first quotation mark
         firstq = string[i].find('"')
+        #location of second quotation mark
         secondq = string[i].find('"', firstq+1)
         #find author
         song.append(string[i][:firstq-1])
         #find title
         song.append(string[i][firstq+1:secondq])
-        #find number of weeks
-        #song.append(string[i][string[i].find('"', firstq+1)+1:string[i].find("n")])
+        #find number of weeks at #1
         song.append(string[i][secondq+2:-1])
         songs.append(song)
-
     return(songs)
 
-print(get_info(rawsongs))
+print(songs1980[1])
 
 #commit!

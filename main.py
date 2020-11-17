b6fe08cab55eec48f59b66d1e09a21babc660780
hankelsesser <72819191+hankelse@users.noreply.github.com>
2020-11-17 17:26:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -71,10 +71,10 @@ def get_length_sucess_data(file, option):
 
 def create_graph(x, y, x1, y1, x2, y2, x3, y3):
     #plt.plot('length of title', 'weeks at #1', data=dataset)
-    plt.plot(x, y, color = "green")
-    plt.plot(x1, y1, color = "blue")
-    plt.plot(x2, y2, color = "yellow")
-    plt.plot(x3, y3, color = "red")
+    plt.stem(x, y, linefmt = "C0-")
+    plt.stem(x1, y1, linefmt = "C1-")
+    plt.stem(x2, y2, linefmt = "C2-")
+    plt.stem(x3, y3, linefmt = "C3-")
 
     plt.xlabel("Characters in a title")
     plt.ylabel("Weeks at #1")
